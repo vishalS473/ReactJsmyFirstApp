63fbc7a13af63dc67a50157967a82d06a9aa7858
vishalS473 <vishaltheboss73@gmail.com>
2024-03-27 01:06:36 +0530

    Revert "Added unit tests for the components"
    This reverts commit 9ca11c33015a3f3a887dc1a1f25637a959d09431.

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 export default function SearchForm({initialQuery,onSearch}) {
-        const handleSearch =(event) =>{
+
+      const handleSearch =(event) =>{
         event.preventDefault();
         onSearch(event.target.Search.value);
       }
@@ -10,8 +11,8 @@ export default function SearchForm({initialQuery,onSearch}) {
         <div className="container my-5">
             <h1 className="my-5">Search Form</h1>
             <form onSubmit={handleSearch}>
-            <input name="Search" type="text" defaultValue ={initialQuery} data-testid="my-input"/>
-            <button type="submit" data-testid="submit-button">Search</button>
+            <input name="Search" type="text" defaultValue ={initialQuery}  />
+            <button type="submit">Search</button>
             <br />
             </form>
         </div>
