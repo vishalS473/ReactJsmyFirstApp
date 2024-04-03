8feaf7bb78708380cbf9e9eeacf522b4225cf21e
vishalS473 <108977310+vishalS473@users.noreply.github.com>
2024-04-03 15:14:42 +0530

    Added unit tests for all components (#3)
    * Added SearchForm and Movie Genre
    
    * Addressed Ratheesh's review comments on Search FOrm amd GenreList
    
    * Added unit tests for the components
    
    * Revert "Added unit tests for the components"
    
    This reverts commit 9ca11c33015a3f3a887dc1a1f25637a959d09431.
    
    * Added Unit Tests for all components
    
    * Added sample E2E Tests
    
    * Reverted changes
    
    * Added E2E Sample Tests

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 
 export default function SearchForm({initialQuery,onSearch}) {
-
-      const handleSearch =(event) =>{
+        const handleSearch =(event) =>{
         event.preventDefault();
         onSearch(event.target.Search.value);
       }
       
     return (
         <div className="container my-5">
-            <h1 className="my-5">Search Form</h1>
+            <h1 className="my-5" data-testid="SearchForm">Search Form</h1>
             <form onSubmit={handleSearch}>
-            <input name="Search" type="text" defaultValue ={initialQuery}  />
-            <button type="submit">Search</button>
+            <input name="Search" type="text" defaultValue ={initialQuery} data-testid="my-input"/>
+            <button type="submit" data-testid="submit-button">Search</button>
             <br />
             </form>
         </div>
